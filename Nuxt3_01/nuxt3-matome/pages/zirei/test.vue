bd27bc5177b3526204337b746a7e05f02c1ccfc8
tossy0130 <tokotoko33ok@gmail.com>
2025-05-26 08:24:28 +0900

    Add_error_25_05

---
+++ Nuxt3_01/nuxt3-matome/pages/zirei/test.vue
@@ -0,0 +1,29 @@
+<tamplate>
+
+</tamplate>
+
+<script setup lang="ts">
+
+
+// エラーになる
+const TestFunc = (value: any): any => { 
+    if(value === '') {
+        return ''
+    } else {
+        return value.substring(0, 5)
+    }
+}
+
+const TestFuncOk = (value: any): string => {
+    if(typeof value === 'string' && value.length >= 10) {
+        return value.substring(0, 5)
+    } else {
+        return '-'
+    }
+}
+
+
+const result01 = TestFunc('abcdefg')
+const result02 = TestFuncOk('abcdefg')
+
+</script>
